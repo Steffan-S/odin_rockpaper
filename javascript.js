688833b344ae7b34fd2d22fdd45ba19070519095
Steffan-S <steffanscholten@hotmail.com>
2023-10-05 21:25:47 +0200

    Created result display
    Created a console log of the score after every round.
    Created an alert with feedback of the round who won and why

--- javascript.js
+++ javascript.js
@@ -1,53 +1,69 @@
 // Computer must know the three choices it can pick Form
 const choices = ["rock", "paper", "scissor"];
-// Computer must choose one of the three choices (random)
+
+// Initialize computer variable
 let computerChoice
+
+// Computer must choose one of the three choices (random)
 function getComputerChoice () {
     let pickComputerChoice = choices[Math.floor((Math.random() * choices.length))];
 // Computer must return the picked choice
     return pickComputerChoice;
 }
 
+// Initialize inputplayer variable
 let inputPlayer
+
 // Player needs to input their choise (rock, paper or scissor)
 // Need to transform input to rule out spelling mistakes
 function getInputPlayer() {
     let pickInputPlayer = prompt('Time to make your choice: Rock, Paper or Scissor').toLowerCase();
+ // Return picked player choise
     return pickInputPlayer;
 }
 
-game();
+
 
 // When we have input from player and computer, compare two inputs
 // Return outcome comparison
 function playRound(player, computer){
-    
-
     console.log(computerChoice + ' The computer');
     console.log(inputPlayer + ' The player');
     if (player == 'rock' && computer == 'paper'){
-        return 'You lose! Paper beats Rock'; 
+        scoreComputer++;
+        return alert('You lose! Paper beats Rock');
     } else if (player == 'rock' && computer == 'scissor'){
-        return 'You win! Rock beats Scissor'; 
+        scorePlayer++;
+        return alert('You win! Rock beats Scissor'); 
     } else if (player == 'paper' && computer == 'rock'){
-        return 'You win! Paper beats rock'; 
+        scorePlayer++;
+        return alert('You win! Paper beats rock'); 
     } else if (player == 'paper' && computer == 'scissor'){
-        return 'You lose! Scissor beats Paper'; 
+        scoreComputer++;
+        return alert('You lose! Scissor beats Paper'); 
     } else if (player == 'scissor' && computer == 'rock'){
-        return 'You lose! Rock beats Scissor'; 
+        scoreComputer++;
+        return alert('You lose! Rock beats Scissor'); 
     } else if (player == 'scissor' && computer == 'paper'){
-        return 'You win! Scissor beats paper'; 
+        scorePlayer++;
+        return alert('You win! Scissor beats paper'); 
+    } else if (player == 'scissor' && computer == 'scissor' || player == 'paper' && computer == 'paper' || player == 'rock' && computer == 'rock'){
+        scoreDraw++;
+        return alert('You Draw!'); 
     } else {
-        return 'You draw!';
+        return alert('You have to pick Rock, Paper or Scissors');
     }
 }
 
-// // Display result for player
+// Display result for player
 // let result = playRound(inputPlayer, resultComputerChoice);
 // alert(result);
 
-
-// // score you and computer
+let scorePlayer = 0;
+let scoreComputer = 0;
+let scoreDraw = 0;
+// console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
+// score you and computer
 // let scorePlayer = 0;
 // let scoreComputer = 0;
 
@@ -57,24 +73,32 @@ function game(){
     computerChoice = getComputerChoice();
     inputPlayer = getInputPlayer();
     playRound(inputPlayer, computerChoice);
+    console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
 
     console.log('starting 2nd round');
     computerChoice = getComputerChoice();
     inputPlayer = getInputPlayer();
     playRound(inputPlayer, computerChoice);
+    console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
 
     console.log('starting 3nd round');
     computerChoice = getComputerChoice();
     inputPlayer = getInputPlayer();
     playRound(inputPlayer, computerChoice);
+    console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
 
     console.log('starting 4nd round');
     computerChoice = getComputerChoice();
     inputPlayer = getInputPlayer();
     playRound(inputPlayer, computerChoice);
+    console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
 
     console.log('starting 5nd round');
     computerChoice = getComputerChoice();
     inputPlayer = getInputPlayer();
     playRound(inputPlayer, computerChoice);
+    console.log('You won: ' + scorePlayer + ' times, the computer won: ' + scoreComputer + ' times and you drawed: ' +scoreDraw + ' times')
 }
+
+// start game loop function
+game();
