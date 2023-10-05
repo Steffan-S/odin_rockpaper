8f06e64b291322cdd34047ab0b4f936a6ef79789
Steffan-S <steffanscholten@hotmail.com>
2023-10-05 20:58:13 +0200

    Creating play loop function
    Getting a 5 game loop to work if you call the game loop
    Player input and computer input not working after first game

--- javascript.js
+++ javascript.js
@@ -1,29 +1,29 @@
 // Computer must know the three choices it can pick Form
 const choices = ["rock", "paper", "scissor"];
-
 // Computer must choose one of the three choices (random)
+let computerChoice = getComputerChoice();
 function getComputerChoice () {
-    let computerChoice = choices[Math.floor((Math.random() * choices.length))];
-    return computerChoice;
+    let pickComputerChoice = choices[Math.floor((Math.random() * choices.length))];
+// Computer must return the picked choice
+    return pickComputerChoice;
 }
 
-// Computer must return the picked choice
-let resultComputerChoice = getComputerChoice();
-
-// console log result function;
-console.log(resultComputerChoice + ' The computer');
-
-
+let inputPlayer = getInputPlayer();
 // Player needs to input their choise (rock, paper or scissor)
-let getInputPlayer = prompt('Time to make your choice: Rock, Paper or Scissor');
-
 // Need to transform input to rule out spelling mistakes
-let inputPlayer = getInputPlayer.toLowerCase();
-console.log(inputPlayer + ' The player');
+function getInputPlayer() {
+    let pickInputPlayer = prompt('Time to make your choice: Rock, Paper or Scissor').toLowerCase();
+    // console.log(inputPlayer + ' inside player function')
+    return pickInputPlayer;
+}
+
+game();
 
 // When we have input from player and computer, compare two inputs
 // Return outcome comparison
 function playRound(player, computer){
+    console.log(computerChoice + ' The computer');
+    console.log(inputPlayer + ' The player');
     if (player == 'rock' && computer == 'paper'){
         return 'You lose! Paper beats Rock'; 
     } else if (player == 'rock' && computer == 'scissor'){
@@ -41,10 +41,37 @@ function playRound(player, computer){
     }
 }
 
-// Display result for player
-let result = playRound(inputPlayer, resultComputerChoice);
-alert(result);
+// // Display result for player
+// let result = playRound(inputPlayer, resultComputerChoice);
+// alert(result);
 
 
+// // score you and computer
+// let scorePlayer = 0;
+// let scoreComputer = 0;
 
-// function game();
+// play 5 games of rock paper scissor
+function game(){
+    console.log('starting the game');
+    playRound(inputPlayer, computerChoice);
+
+    console.log('starting 2nd round');
+    getComputerChoice();
+    getInputPlayer();
+    playRound(inputPlayer, computerChoice);
+
+    console.log('starting 3nd round');
+    getComputerChoice();
+    getInputPlayer();
+    playRound(inputPlayer, computerChoice);
+
+    console.log('starting 4nd round');
+    getComputerChoice();
+    getInputPlayer();
+    playRound(inputPlayer, computerChoice);
+
+    console.log('starting 5nd round');
+    getComputerChoice();
+    getInputPlayer();
+    playRound(inputPlayer, computerChoice);
+}
